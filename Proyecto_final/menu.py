435907a1228573012823cc54b0e4e3133fda80be
Cristianrage <cristianrage1@gmail.com>
2024-10-03 20:13:55 -0300

    Armados de menu

---
+++ Proyecto_final/menu.py
@@ -0,0 +1,15 @@
+#Menu inicial
+
+print("Menu de gestion de productos:\n")
+
+print("1. Alta de nuevos productos.")
+print("2. Consulta de datos de productos.")
+print("3. Modificar cantidad de stock de un producto.")
+print("4. Dar de baja productos.")
+print("5. Listado de completo de productos.")
+print("6. Lista productos con bajo stock")
+print("7. Salir")
+
+opcion = int(input("Seleccione una opción entre 1 y 7: "))
+
+print(f"Has seleccionado la opción {opcion}")
