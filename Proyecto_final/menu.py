e1825f0d87117272d7cea702dc8882f49410559e
Cristianrage <cristianrage1@gmail.com>
2024-12-11 23:56:39 -0300

    added some functions

--- Proyecto_final/menu.py
+++ Proyecto_final/menu.py
@@ -1,8 +1,45 @@
 #Menu inicial
 import os
 
-titulos = ["Nombre", "Cantidad", "Precio unitario"]
-productos = []
+titulos = ["id","Nombre", "Descripcion", "Precio", "Cantidad", "Categoria"]
+
+Producto1 = {"nombre" : "manzana", "descripcion" : "deliciosa", "precio" : 2000.00, "cantidad" : 100, "categoria" : "fruta"}
+Producto2 = {"nombre" : "mayonesa", "descripcion" : "BC", "precio" : 1800.00, "cantidad" : 100, "categoria" : "aderezo"}
+
+productos = {1 : Producto1, 2 : Producto2}
+
+idcount = 3
+
+def ingresar_precio():
+
+    no_es_real = True
+    while(no_es_real):
+        try:
+            precio = float(input("Ingrese el precio unitario: "))
+        except ValueError:
+            print("Por favor ingrese un número \n")
+        else:
+            no_es_real = False
+            return precio
+
+def ingresar_cantidad():
+
+    no_es_entero = True
+    while(no_es_entero):
+        try:
+            cantidad = int(input("Ingrese la cantidad del producto: "))
+        except ValueError:
+            print("Por favor ingrese un número entero como cantidad \n")
+        else:
+            no_es_entero = False
+            return cantidad
+
+def registrar_producto() :
+    nombre = input("Ingrese el nombre del producto: ")
+    descripcion = input("Ingrese la descripción del producto: ")
+    precio = ingresar_precio()
+    cantidad = ingresar_cantidad()
+    productos.append([nombre, cantidad, precio])
 
 opcion = 0
 while opcion != 7: 
@@ -20,10 +57,7 @@ while opcion != 7:
     os.system('cls')
     print(f"Has seleccionado la opción {opcion}")
     if(opcion == 1):
-        nombre = input("Ingrese el nombre del producto: ")
-        cantidad = int(input("Ingrese la cantidad del producto: "))
-        precio = float(input("Ingrese el precio unitario: "))
-        productos.append([nombre, cantidad, precio])
+        registrar_producto()
     elif(opcion == 2):
         print(f"{titulos[0]}\t\t{titulos[1]}\t{titulos[2]}")
         for producto in productos:
